07237164f9b1fcb5bd6d6d18d915f43f6aad9aa3
Derick Silva <dericksilva@live.com>
2021-01-14 10:07:38 -0300

    Update ex010.py

--- ws-python/ex010.py
+++ ws-python/ex010.py
@@ -1,3 +1,3 @@
 reais = float(input('Digite o valor em reais R$ '))
 dolar = reais / 3.27
-print(f'Com R$ {reais} voce pode comprar US${dolar:.2f}')
+print(f'Com R$ {reais:.2f} voce pode comprar US${dolar:.2f}')
